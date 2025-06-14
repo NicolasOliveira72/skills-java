a68bed9a468ef8b93a6dfaf89e5dc6094c47cb3c
nicolas <nicolas1421rocha@gmail.com>
2025-06-14 19:09:42 -0400

    :bricks: ci: If else

---
+++ IfElse.java
@@ -0,0 +1,25 @@
+class IfElse {
+    public static void main (String[] args){    
+
+    int x = 5;
+
+    if (x == 1) {
+        System.out.println("Resultado é 1");
+    } else if (x == 2) {
+        System.out.println("Resultado é 2");
+    } else if (x == 3) {
+        System.out.println("Resultado é 3");
+    } else if (x == 4) { 
+        System.out.println("Resultado é 4");
+    } else if (x == 5) {
+        System.out.println("Resultado é 5");
+    } else if (x == 6) {
+        System.out.println("Resultado é 6");
+    } else if (x == 7) {
+        System.out.println("Resultado é 7");
+    } else {
+        System.out.println("Nenhum resultdo");
+   }
+
+}
+}
