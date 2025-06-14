1bf7a3c564de37f50614690efc34bd967e3d626a
nicolas <nicolas1421rocha@gmail.com>
2025-06-14 17:18:38 -0400

    :bricks: ci: soma de dois valores

---
+++ Soma.java
@@ -0,0 +1,8 @@
+class Soma {
+    public static void main (String[] args){
+        int x = 10;
+        int y = 20;
+        int resultado = x + y;
+        System.out.println("O resultado é " + resultado);
+    }
+}
