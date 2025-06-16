fa16c8641eae3ac62d1fe979bc67a89a9307341a
nicolas <nicolas1421rocha@gmail.com>
2025-06-16 10:11:55 -0400

    :bricks: ci: adicionando operador ternário

---
+++ Ternario.java
@@ -0,0 +1,12 @@
+// Algoritmo de operador ternário:
+
+public class Ternario {
+    public static void main(String[] args){
+        // Variável "x" declarada para comparação:
+        int x = 14;
+        // Operador ternário para comparar com a variável declarada:
+        String resultado = (x == 12) ? "Correto" : "Errado";
+        // Imprimindo o resultado da análise:
+        System.out.println(" A resposra é: " + resultado);
+    }
+}
