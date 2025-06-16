011f323671aa705023e2b85ab82aa48a69c59e6c
nicolas <nicolas1421rocha@gmail.com>
2025-06-16 10:33:51 -0400

    :bricks: ci: adicionando switch case

---
+++ Switch.java
@@ -0,0 +1,23 @@
+// Algoritmo de estrutura condicinonal switch:
+
+public class Switch {
+    public static void main (String[] args){
+        int x = 15;
+        switch (x){
+            case 9:
+            System.out.println("O valor é: 9");
+            break;
+            case 10:
+            System.out.println("O valor é: 10");
+            break;
+            case 12:
+            System.out.println("O valor é: 12");
+            break;
+            case 15:
+            System.out.println("O valor é: 15");
+            break;
+            default: 
+            System.out.println("Nenhum valor");
+        }
+    }
+}
