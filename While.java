6900d111650e139c714d3dc9bc06c298b8800089
nicolas <nicolas1421rocha@gmail.com>
2025-06-20 19:41:25 -0400

    :sparkles: while

---
+++ While.java
@@ -0,0 +1,13 @@
+// Algoritmo com estrutura de repetição while:
+public class While {
+    public static void main (String[] args){
+        // Declaração da variável "x" para a execução do while:
+        int x = 5;
+        // While com a variável "x" declarada:
+        while (x <= 10) {
+            System.out.println("O número é: " + x);
+            System.out.println("-----------------");
+            x++;
+        }
+    }
+}
