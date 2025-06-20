76ea823e8215fc2f97f9aecf6a20fee4a78b8904
nicolas <nicolas1421rocha@gmail.com>
2025-06-20 19:47:52 -0400

    books: documntação do for

--- For.java
+++ For.java
@@ -1,5 +1,7 @@
+// Algoritmo para a execução da estrutura de repetição "for":
 class For {
     public static void main (String[] args){
+        //Bloco de execução do for: 
         for (int x = 5; x <= 10; x++){
             System.out.println("Número: " + x);
 
